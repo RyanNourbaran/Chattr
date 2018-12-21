a647932b144c5ba3631e0b4db8c57097ad404752
Ryan Nourbaran <ryan.nourbaran@hotmail.com>
2018-12-21 00:43:29 -0500

    chat screen layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./C/Title";
+import MessageList from "./C/MessageList";
+import SendMessageForm from "./C/SendMessageForm";
 
-class App extends Component {
+const DUMMY_DATA = [
+  {
+    senderId: "perborgen",
+    text: "who'll win?"
+  },
+  {
+    senderId: "janedoe",
+    text: "who'll win?"
+  }
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: DUMMY_DATA
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <Title />
+        <MessageList messages={this.state.messages} />
+        <SendMessageForm />
       </div>
     );
   }
---
+++ src/C/MessageList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class MessageList extends React.Component {
+  render() {
+    return (
+      <ul className="message-list">
+        {this.props.messages.map(message => {
+          return (
+            <li key={message.id}>
+              <div>{message.senderId}</div>
+              <div>{message.text}</div>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
